a8fc27cc410bf83c49810fd402d203e37eac3e66
Khesa Alvandi Sembodo <44808111+masesas@users.noreply.github.com>
2021-02-28 18:40:25 +0700

    Structure Build

--- App/app/src/main/java/com/example/tebakkatagame/Activity/MainActivity.java
+++ App/app/src/main/java/com/example/tebakkatagame/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tebakkatagame;
+package com.example.tebakkatagame.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tebakkatagame.R;
 
 public class MainActivity extends AppCompatActivity {
 
