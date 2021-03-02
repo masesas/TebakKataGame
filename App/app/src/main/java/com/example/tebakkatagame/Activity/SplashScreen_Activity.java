78967006504d285b4e962f7f63e991bd27d2420f
Ednan <ednanm22@gmail.com>
2021-03-02 23:26:46 +0700

    Asset Add

---
+++ App/app/src/main/java/com/example/tebakkatagame/Activity/SplashScreen_Activity.java
@@ -0,0 +1,16 @@
+package com.example.tebakkatagame.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tebakkatagame.R;
+
+public class SplashScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_);
+    }
+}
