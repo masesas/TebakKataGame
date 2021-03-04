47f4572040fd1213735317a93efb5b86e56be261
Ednan <ednanm22@gmail.com>
2021-03-04 23:19:50 +0700

    update

--- App/app/src/main/java/com/example/tebakkatagame/Activity/SplashScreen_Activity.java
+++ App/app/src/main/java/com/example/tebakkatagame/Activity/SplashScreen_Activity.java
@@ -1,16 +1,34 @@
 package com.example.tebakkatagame.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.tebakkatagame.R;
 
-public class SplashScreen_Activity extends AppCompatActivity {
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+public class SplashScreen_Activity extends BaseApp {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_);
+
+        final GifImageView gifView = (GifImageView) findViewById(R.id.gif);
+        try {
+            GifDrawable gifDrawable = new GifDrawable(getResources().openRawResource(R.raw.sukukatagif));
+            gifView.setImageDrawable(gifDrawable);
+
+        }catch (Exception e){}
+
+        find(R.id.ly_splash).postDelayed(() -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        },2950);
     }
+
+
+
 }
