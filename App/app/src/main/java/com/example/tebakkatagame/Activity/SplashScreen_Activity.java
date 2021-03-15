2b3e4090af9a772f4a791178aba921cc7f536245
Khesa Alvandi Sembodo <44808111+masesas@users.noreply.github.com>
2021-03-16 02:26:32 +0700

    update asset

--- App/app/src/main/java/com/example/tebakkatagame/Activity/SplashScreen_Activity.java
+++ App/app/src/main/java/com/example/tebakkatagame/Activity/SplashScreen_Activity.java
@@ -1,47 +1,34 @@
 package com.example.tebakkatagame.Activity;
 
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity;
 
-=======
 import android.content.Intent;
->>>>>>> 3dfb6e9c5c15db82950a5d46f2f08bf85fcad53d
 import android.os.Bundle;
 
 import com.example.tebakkatagame.R;
 
-<<<<<<< HEAD
-public class SplashScreen_Activity extends AppCompatActivity {
-=======
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
 public class SplashScreen_Activity extends BaseApp {
->>>>>>> 3dfb6e9c5c15db82950a5d46f2f08bf85fcad53d
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_);
-<<<<<<< HEAD
-    }
-=======
-
         final GifImageView gifView = (GifImageView) findViewById(R.id.gif);
         try {
             GifDrawable gifDrawable = new GifDrawable(getResources().openRawResource(R.raw.sukukatagif));
             gifView.setImageDrawable(gifDrawable);
 
-        }catch (Exception e){}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         find(R.id.ly_splash).postDelayed(() -> {
             Intent intent = new Intent(getActivity(), MainActivity.class);
             startActivity(intent);
             finish();
-        },2950);
+        }, 2950);
     }
-
-
-
->>>>>>> 3dfb6e9c5c15db82950a5d46f2f08bf85fcad53d
 }
